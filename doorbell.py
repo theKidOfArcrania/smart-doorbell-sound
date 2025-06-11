850765f67657731153f725dd5d109d543a596d95
Henry Wang <theKidOfArcrania@users.noreply.github.com>
2025-06-11 10:41:15 -0700

    Smoother auth flow

--- doorbell.py
+++ doorbell.py
@@ -5,8 +5,10 @@ import requests
 import subprocess
 
 from concurrent.futures import TimeoutError
+from http.server import BaseHttpRequestHandler, HTTPServer
 from google.cloud import pubsub_v1
-from urllib.parse import urlencode
+from subprocess import check_output
+from urllib.parse import urlencode, urlparse, parse_qs
 
 ap = argparse.ArgumentParser(description='Runs a doorbell sound from a smart doorbell')
 ap.add_argument('--test', action='store_true', help='test doorbell sound at start')
@@ -17,8 +19,69 @@ aplay_group.add_argument('file', metavar='FILE', help='sound file to play for do
 base = 'https://smartdevicemanagement.googleapis.com/v1'
 base_ent = f'{base}/enterprises/{auth.project_id}'
 
+def get_ip():
+    output = check_output("ip addr show dev wlan0 | grep 'inet ' | awk -F '/' -F ' +|/' '{ print $3 }'", shell=True)
+    return str(output.strip(), 'utf8')
+
+class AuthFlow:
+    def __init__(self):
+        outer = self
+
+        url_query = urlencode({
+            'redirect_uri': 'https://www.google.com',
+            'access_type': 'offline',
+            'prompt': 'consent',
+            'client_id': auth.client_id,
+            'response_type': 'code',
+            'scope': 'https://www.googleapis.com/auth/sdm.service',
+        })
+        auth_url = f'https://nestservices.google.com/partnerconnections/{auth.project_id}/auth?{url_query}'
+
+        class AuthFlowHandler(BaseHTTPRequestHandler):
+            def log_message(self, *args, **kwargs):
+                pass
+
+            def do_GET(self):
+                outer._code
+                url = urlparse(self.path)
+
+                if url.path == '/':
+                    self.send_response(307)
+                    self.send_headers('location', url)
+                    self.end_headers()
+                    self.wfile.write(bytes(f'Redirected to {url}', 'utf8'))
+                elif url.path == '/auth':
+                    qs = parse_qs(url.query)
+                    error = qs.get('error')
+                    if error == None:
+                        self.send_response(200)
+                        self.end_headers()
+                        self.wfile.write(bytes('Success! You can now close this tab.', 'utf8'))
+                        code = qs.get('code')[0]
+                    else:
+                        self.send_response(403)
+                        self.end_headers()
+                        msg = f'Error: {error[0]}\n{"".join(qs.get("error_description", []))}'
+                        self.wfile.write(bytes(msg, 'utf8'))
+                else:
+                    self.send_response(404)
+                    self.end_headers()
+
+        self._code = None
+        self.__handler = AuthFlowHandler
+
+    def launch_auth(self):
+        ip = get_ip()
+        httpd = HTTPServer((ip, 3000), self.__handler)
+        print(f'Visit http://{ip}:3000/')
+        self._code = None
+        while self._code == None:
+            httpd.handle_request()
+        return self._code
+
+redirect_uri = f'https://redirect.thekidofarcrania.com/{ip}:3000/auth'
 def obtain_token():
-    global auth_code
+    auth_code = AuthFlow().launch_auth()
     r = requests.post(
         'https://www.googleapis.com/oauth2/v4/token',
         params = {
@@ -26,7 +89,7 @@ def obtain_token():
             'client_secret': auth.client_secret,
             'code': auth_code,
             'grant_type': 'authorization_code',
-            'redirect_uri': 'https://www.google.com',
+            'redirect_uri': redirect_uri,
         }
     )
     return r.json()
@@ -48,10 +111,19 @@ token = None
 def api_function(request_fn):
     global token
     if token == None:
-        token = obtain_token()
+        try:
+            token = json.load(open('token', 'r'))
+            refresh_token(token)
+        except:
+            print('Refresh auth failed... Trying authenticating')
+            token = obtain_token()
+        with open('token', 'w') as f:
+            json.dump(token, f)
     data = request_fn(token)
     if 'error' in data:
         token = refresh_token(token)
+        with open('token', 'w') as f:
+            json.dump(token, f)
         data = request_fn(token)
         if 'error' in data:
             print(data)
@@ -94,24 +166,13 @@ def callback(message: pubsub_v1.subscriber.message.Message) -> None:
         play_doorbell()
 
 def main():
-    global auth_code
     global ns
     ns = ap.parse_args()
 
     if ns.test:
         play_doorbell()
 
-    url_query = urlencode({
-        'redirect_uri': 'https://www.google.com',
-        'access_type': 'offline',
-        'prompt': 'consent',
-        'client_id': auth.client_id,
-        'response_type': 'code',
-        'scope': 'https://www.googleapis.com/auth/sdm.service',
-    })
-    url = 'https://accounts.google.com/o/oauth2/v2/auth?' + url_query
-    print(f'Visit the url at: {url}')
-    auth_code = input('Code: ')
+    print(api_function(list_devices))
 
     subscriber = pubsub_v1.SubscriberClient()
     subscription_path = subscriber.subscription_path(
@@ -126,7 +187,7 @@ def main():
     with subscriber:
         while True:
             try:
-                streaming_pull_future.result(60 * 60 * 24)
+                streaming_pull_future.result(5)
             except TimeoutError:
                 pass
 
